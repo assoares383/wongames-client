5ec6bd2b5140fc9b21207a1c81b54c32a4b0f178
= <alexandressoares383@gmail.com>
2024-04-05 20:51:42 -0300

    feat: melhorando acessibilidade e primeiros testes

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,28 @@
+import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
+import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
+import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
+
+import Logo from '../Logo'
+
+import * as S from './styles'
+
+const Menu = () => (
+  <S.Wrapper>
+    <S.IconWrapper>
+      <MenuIcon aria-label="Open Menu" />
+    </S.IconWrapper>
+    <S.LogoWrapper>
+      <Logo hideOnMobile />
+    </S.LogoWrapper>
+    <S.MenuGroup>
+      <S.IconWrapper>
+        <SearchIcon aria-label="Search" />
+      </S.IconWrapper>
+      <S.IconWrapper>
+        <ShoppingCartIcon aria-label="Open Shopping Cart" />
+      </S.IconWrapper>
+    </S.MenuGroup>
+  </S.Wrapper>
+)
+
+export default Menu
