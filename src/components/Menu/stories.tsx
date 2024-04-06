d41b6313bead07e8db8da213afd50901f0b8478c
= <alexandressoares383@gmail.com>
2024-04-06 20:35:42 -0300

    feat: criando regra para usuario logado/deslogado

--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -1,12 +1,12 @@
 import { Meta } from '@storybook/react'
-import Menu from '.'
+import Menu, { MenuProps } from '.'
 
 export default {
   title: 'Menu',
   component: Menu
 } as Meta
 
-export const Default = () => <Menu />
+export const Default = (args: MenuProps) => <Menu {...args} />
 
 Default.parameters = {
   layout: 'fullscreen',
